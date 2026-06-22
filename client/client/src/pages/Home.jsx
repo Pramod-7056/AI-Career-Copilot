070d92cd049e0bbed465502477884d1c53f408a3
PRAMOD M C <pramuprashi123@gmail.com>
2026-06-22 11:16:08 +0530

    first commit

---
+++ client/client/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { useEffect,useState } from "react";
+
+function App(){
+  const [message,setMessage]=useState("")
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/health")
+    .then((res)=>res.json())
+    .then((data)=>{
+      setMessage(data)
+      
+      
+      
+    })
+    
+    
+    .catch(()=>{
+      setMessage("Backend not Connected")
+    })
+  },[])
+  return(
+    <div>
+      <h1>AI Career Copilot</h1>
+      <p>Your Ai powered placement preparation companion.</p>
+      <h3>Backend Status:{message.status}</h3>
+      <h4>Backend Version:{message.version}</h4>
+      <button>Get Started</button>
+    </div>
+  )
+}
+
+export default App;
---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const cors=require('cors')
+
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/api/health",(req,res)=>{
+    res.json({
+        status:"working",
+        project:"AI Career  Copilot",
+       version:"1.0"
+    })
+})
+
+const PORT=5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
