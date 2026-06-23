78bae407addaf65a2cada19b7ddc275b5f7b7bab
PRAMOD M C <pramuprashi123@gmail.com>
2026-06-23 10:45:38 +0530

    React-Router-setup

---
+++ client/client/src/pages/About.jsx
@@ -0,0 +1,4 @@
+function About(){
+    return (<h1>About US</h1>)
+}
+export default About;
---
+++ client/client/src/pages/login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return(
+        <div>
+            <h1>Login page</h1>
+            <h3>Enter Name</h3>
+            <button>Login</button>
+        </div>
+    )
+}
+export default Login;
