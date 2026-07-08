ee44111f1f926270a62e1dcccb4456e1321f8a38
PRAMOD M C <pramuprashi123@gmail.com>
2026-07-08 15:27:25 +0530

    dashboard integration

--- client/client/src/pages/Dashboard.jsx
+++ client/client/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { useState,useEffect } from "react";
+import "./Dashboard.css";
 
 
 function Dashboard(){
 
     const [goals,setGoals]=useState([])
+    const [title,setTitle]=useState("")
 
     const fetchGoals = async () => {
     try {
@@ -25,25 +27,109 @@ function Dashboard(){
     }
 };
 
+const addGoal=async()=>{
+    try{
+        const token=localStorage.getItem("token");
+        await fetch("http://localhost:5000/api/goals",{
+            method:"POST",
+            headers:{
+                "Content-Type":"Application/json",
+                Authorization:`Bearer ${token}`
+            },
+            body:JSON.stringify({
+                title
+            })
+        })
+        setTitle("")
+        fetchGoals()
+    }
+
+catch(error){
+   console.log(error)
+}
+}
+
+const completeGaol=async(id)=>{
+    try{
+        const token=localStorage.getItem("token");
+        await fetch(`http://localhost:5000/api/goals/${id}`,{
+            method:"PUT",
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+        })
+        fetchGoals()
+    }
+
+catch(error){
+   console.log(error)
+}
+}
+
+
+const deleteGaol=async(id)=>{
+    try{
+        const token=localStorage.getItem("token");
+        await fetch(`http://localhost:5000/api/goals/${id}`,{
+            method:"DELETE",
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+        })
+        fetchGoals()
+    }
+
+catch(error){
+   console.log(error)
+}
+}
+
     useEffect(()=>{
         
    fetchGoals();
     },[])
-    return(
+    
+        return (
+  <div className="dashboard">
+    <h1>Welcome to your Dashboard</h1>
+
+    <div className="goal-input">
+    <input
+        type="text"
+        placeholder="Enter a goal..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+    />
+
+    <button onClick={addGoal}>
+        +Add Goal
+    </button>
+</div>
+
+    <div className="header">
+
+    
+      <h2>My Goals</h2>
+      
+    </div>
+
+    {goals.map((goal) => (
+      <div className="goal-card" key={goal._id}>
         <div>
-            <h1>Welcome to ur Dashboard</h1>
-            
+          <h3>{goal.title}</h3>
+          <p>{goal.completed ? "✅ Completed" : "⏳ Pending"}</p>
+        </div>
 
-            <h2>My Goals</h2>
+        <div className="actions">
+          <button className="complete-btn" onClick={()=>completeGaol(goal._id)}>Complete</button>
+          <button className="delete-btn" onClick={()=>deleteGaol(goal._id
 
-            {goals.map((goal)=>(
-                <div key={goal._id}>
-                    <p>{goal.title}</p>
-                    </div>
-            ))}
-            </div>
-        
-    )
+          )}>Delete</button>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 }
 
 export default Dashboard;
